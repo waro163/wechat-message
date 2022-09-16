27647517a723cf75e5167cf6e50bbd9b05955129
Wang Rong <wangron@tesla.com>
2022-09-16 18:20:23 +0800

    update text message

--- mp/message/text.go
+++ mp/message/text.go
@@ -1,5 +1,7 @@
 package message
 
+import "github.com/waro163/wechat-message/utils"
+
 // Text 文本消息
 type Text struct {
 	CommonMsg
@@ -10,5 +12,7 @@ type Text struct {
 func NewText(content string) *Text {
 	text := new(Text)
 	text.Content = CDATA(content)
+	text.SetMsgType(MsgTypeText)
+	text.SetCreateTime(utils.GetCurrTS())
 	return text
 }
