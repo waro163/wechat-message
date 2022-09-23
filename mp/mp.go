270495a94b0bc1d3fc3a3ee1054a181309a7edb3
Wang Rong <wangron@tesla.com>
2022-09-23 18:25:08 +0800

    add validation signature function

--- mp/mp.go
+++ mp/mp.go
@@ -1,1 +1,29 @@
 package mp
+
+import (
+	"crypto/sha1"
+	"fmt"
+	"io"
+	"net/http"
+	"sort"
+)
+
+func Validate(req *http.Request, token string, skip bool) bool {
+	if skip {
+		return true
+	}
+	query := req.URL.Query()
+	timestamp := query.Get("timestamp")
+	nonce := query.Get("nonce")
+	signature := query.Get("signature")
+	return signature == Signature(token, timestamp, nonce)
+}
+
+func Signature(params ...string) string {
+	sort.Strings(params)
+	h := sha1.New()
+	for _, s := range params {
+		_, _ = io.WriteString(h, s)
+	}
+	return fmt.Sprintf("%x", h.Sum(nil))
+}
