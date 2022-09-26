32b1d9f60b9f0fd16e3bc65b540012d210ab813e
Wang Rong <wangron@tesla.com>
2022-09-26 18:40:09 +0800

    reconstruct mixmessage struct

--- mp/message.go
+++ mp/message.go
@@ -33,42 +33,87 @@ type MixMessage struct {
 	msg.CommonMsg
 
 	// 基本消息
-	MsgID        int64   `xml:"MsgId"`        //text,image,voice,video,location,link
-	Content      string  `xml:"Content"`      //text,
-	Recognition  string  `xml:"Recognition"`  //voice,
-	PicURL       string  `xml:"PicUrl"`       //image,
-	MediaID      string  `xml:"MediaId"`      //image,voice,video
-	Format       string  `xml:"Format"`       //voice,
-	ThumbMediaID string  `xml:"ThumbMediaId"` //video,
-	LocationX    float64 `xml:"Location_X"`   //location,
-	LocationY    float64 `xml:"Location_Y"`   //location,
-	Scale        float64 `xml:"Scale"`        //location,
-	Label        string  `xml:"Label"`        //location,
-	Title        string  `xml:"Title"`        //link
-	Description  string  `xml:"Description"`  //link
-	URL          string  `xml:"Url"`          //link
+	// --------text--------
+	MsgID   int64  `xml:"MsgId"`   //text,
+	Content string `xml:"Content"` //text,
+
+	// --------image--------
+	// MsgID        int64   `xml:"MsgId"`
+	PicURL  string `xml:"PicUrl"`  //image,
+	MediaID string `xml:"MediaId"` //image
+
+	// --------voice--------
+	// MsgID        int64   `xml:"MsgId"`
+	// MediaID      string  `xml:"MediaId"`//voice
+	Format      string `xml:"Format"`      //voice,
+	Recognition string `xml:"Recognition"` //voice,
+
+	// --------video/shortvideo--------
+	// MsgID        int64   `xml:"MsgId"`
+	// MediaID      string  `xml:"MediaId"`  //video
+	ThumbMediaID string `xml:"ThumbMediaId"` //video,
+
+	// --------location--------
+	// MsgID        int64   `xml:"MsgId"`
+	LocationX float64 `xml:"Location_X"` //location,
+	LocationY float64 `xml:"Location_Y"` //location,
+	Scale     float64 `xml:"Scale"`      //location,
+	Label     string  `xml:"Label"`      //location,
+
+	// --------link--------
+	// MsgID        int64   `xml:"MsgId"`
+	Title       string `xml:"Title"`       //link
+	Description string `xml:"Description"` //link
+	URL         string `xml:"Url"`         //link
 
 	// 事件相关
-	TemplateMsgID int64     `xml:"MsgID"` // 模板消息推送成功的消息是MsgID, template
-	Event         EventType `xml:"Event" json:"Event"`
-	EventKey      string    `xml:"EventKey"`  //scan_qr,click_menu,
-	Ticket        string    `xml:"Ticket"`    //scan,subscribe
-	Latitude      string    `xml:"Latitude"`  //report_location
-	Longitude     string    `xml:"Longitude"` //report_location
-	Precision     string    `xml:"Precision"` //report_location
-	MenuID        string    `xml:"MenuId"`    //view-mini
-	Status        string    `xml:"Status"`    //template
+	// --------关注/取消关注--------
+	Event EventType `xml:"Event" json:"Event"`
 
+	// --------scan qr--------
+	// Event         EventType `xml:"Event"`
+	EventKey string `xml:"EventKey"` //scan_qr,click_menu,
+	Ticket   string `xml:"Ticket"`   //scan,subscribe
+
+	// --------report_location--------
+	// Event         EventType `xml:"Event"`
+	Latitude  string `xml:"Latitude"`  //report_location
+	Longitude string `xml:"Longitude"` //report_location
+	Precision string `xml:"Precision"` //report_location
+
+	// --------custom menu--------
+	// Event EventType `xml:"Event"`
+	// EventKey string `xml:"EventKey"`
+
+	// --------template message--------
+	// Event EventType `xml:"Event"`
+	TemplateMsgID int64  `xml:"MsgID"`  //MsgID
+	Status        string `xml:"Status"` //template
+
+	// --------menu view link/mini--------
+	// Event EventType `xml:"Event"`
+	// EventKey string `xml:"EventKey"`
+	MenuID string `xml:"MenuId"` //view-mini
+
+	// --------scan code push--------
+	// Event EventType `xml:"Event"`
+	// EventKey string `xml:"EventKey"
 	ScanCodeInfo struct {
 		ScanType   string `xml:"ScanType"`
 		ScanResult string `xml:"ScanResult"`
 	} `xml:"ScanCodeInfo"` //scan_push
 
+	// --------pic--------
+	// Event EventType `xml:"Event"`
+	// EventKey string `xml:"EventKey"
 	SendPicsInfo struct {
 		Count   int32      `xml:"Count"`
 		PicList []EventPic `xml:"PicList>item"`
 	} `xml:"SendPicsInfo"`
 
+	// --------location select--------
+	// Event EventType `xml:"Event"`
+	// EventKey string `xml:"EventKey"
 	SendLocationInfo struct {
 		LocationX float64 `xml:"Location_X"`
 		LocationY float64 `xml:"Location_Y"`
@@ -77,13 +122,19 @@ type MixMessage struct {
 		Poiname   string  `xml:"Poiname"`
 	} `xml:"SendLocationInfo"`
 
+	// --------订阅弹窗--------
+	// Event EventType `xml:"Event"`
 	SubscribeMsgPopupEventList []SubscribeMsgPopupEvent `xml:"SubscribeMsgPopupEvent>List"` //subscribe_msg_popup_event
 
+	//  --------管理订阅--------
+	// Event EventType `xml:"Event"`
 	SubscribeMsgChangeEvent struct {
 		TemplateID            string `xml:"TemplateId"`
 		SubscribeStatusString string `xml:"SubscribeStatusString"`
 	} `xml:"SubscribeMsgChangeEvent>List"` // subscribe_msg_change_event
 
+	//  -------- bizsend 接口发送--------
+	// Event EventType `xml:"Event"`
 	SubscribeMsgSentEvent struct {
 		TemplateID  string `xml:"TemplateId"`
 		MsgID       int64  `xml:"MsgID"`
