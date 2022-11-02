f9423deccb1da0a8d6e3f1bed1ea8e0657f3b1a7
Wang Rong <wangron@tesla.com>
2022-11-02 15:47:14 +0800

    add testcase

--- mp/check/push-event-mass-job_test.go
+++ mp/check/push-event-mass-job_test.go
@@ -4,14 +4,21 @@ import (
 	"encoding/xml"
 	"testing"
 
+	"github.com/stretchr/testify/assert"
 	"github.com/waro163/wechat-message/mp"
+	msg "github.com/waro163/wechat-message/mp/message"
 )
 
 func TestMassJobEvent(t *testing.T) {
 	var res mp.MixMessage
 	err := xml.Unmarshal([]byte(MassJobEventDemo), &res)
-	if err != nil {
-		t.Error("not suit for MassJobEventDemo type", err)
-	}
-	t.Log(res)
+	assert.Nil(t, err)
+	assert.Equal(t, msg.MsgTypeEvent, res.MsgType)
+	assert.Equal(t, mp.EventMassSendJobFinish, res.Event)
+	assert.NotNil(t, res.CopyrightCheckResult)
+	assert.NotNil(t, res.ArticleUrlResult)
+	// if err != nil {
+	// 	t.Error("not suit for MassJobEventDemo type", err)
+	// }
+	// t.Log(res)
 }
