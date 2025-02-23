cf1746882c1ed51396a931b7d8b1ce6509d0fe9b
Wang Rong <wangron@tesla.com>
2025-02-23 13:50:34 +0800

    [perf] add new function to handle msg

--- _example/httpserver/main.go
+++ _example/httpserver/main.go
@@ -8,19 +8,17 @@ import (
 	"github.com/waro163/wechat-message/mp/serve"
 )
 
-type handleMsg struct{}
-
-func (h *handleMsg) HandleMsg(mp.MixMessage) (interface{}, error) {
+var msgHander = serve.NewCustomMessageHandler(func(mm mp.MixMessage) (interface{}, error) {
 	text := message.NewText("you got it!")
 	return text, nil
-}
+})
 
 func main() {
 	handle := serve.Server{
 		Token:        "",
 		SkipValidata: true,
 		LogMsg:       true,
-		Handler:      &handleMsg{},
+		Handler:      msgHander,
 	}
 
 	http.HandleFunc("/wechat/callback", handle.ServeHTTP)
--- mp/serve/handle-msg.go
+++ mp/serve/handle-msg.go
@@ -5,3 +5,17 @@ import "github.com/waro163/wechat-message/mp"
 type IMessageHandler interface {
 	HandleMsg(mp.MixMessage) (interface{}, error)
 }
+
+type customMessagehandler struct {
+	msgHandler func(mp.MixMessage) (interface{}, error)
+}
+
+func NewCustomMessageHandler(msgHandler func(mp.MixMessage) (interface{}, error)) IMessageHandler {
+	return &customMessagehandler{
+		msgHandler: msgHandler,
+	}
+}
+
+func (h *customMessagehandler) HandleMsg(msg mp.MixMessage) (interface{}, error) {
+	return h.msgHandler(msg)
+}
